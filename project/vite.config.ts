db51a2b74103bd2165d0600b9508bd1e29a7a0c5
adi-Bernat <dybrnt28@gmail.com>
2025-06-16 17:10:19 +0300

    Update vite.config.ts to import package.json and add app version define

--- project/vite.config.ts
+++ project/vite.config.ts
@@ -1,9 +1,17 @@
-import tailwindcss from "@tailwindcss/vite";
-import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
 import flowbiteReact from "flowbite-react/plugin/vite";
 
-// https://vite.dev/config/
+
+import pkg from "./package.json";
+
+
+
 export default defineConfig({
   plugins: [react(), tailwindcss(), flowbiteReact()],
+  define: {
+    __APP_VERSION__: JSON.stringify(pkg.version),
+    __APP_NAME__: JSON.stringify(pkg.name),
+  },
 });
