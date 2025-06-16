129e7b124f0e30ff85b658cc741cda1ede5ed24a
adi-Bernat <dybrnt28@gmail.com>
2025-06-16 19:44:35 +0300

    עדכון נתיב package.json בקובץ vite.config.ts

--- project/vite.config.ts
+++ project/vite.config.ts
@@ -2,9 +2,7 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 import flowbiteReact from "flowbite-react/plugin/vite";
-
-
-import pkg from "./package.json";
+import pkg from '../package.json';
 
 
 
