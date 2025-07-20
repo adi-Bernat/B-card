d5781f7f030ea9335b9e93960671b7c6d1cdc96c
adi-Bernat <dybrnt28@gmail.com>
2025-07-20 12:47:42 +0300

    Add package.Json and update tsconfig.node.json and vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,8 +2,11 @@ import tailwindcss from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 import flowbiteReact from "flowbite-react/plugin/vite";
+import pkg from './package.json';
+console.log(pkg.version); // לדוגמה
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss(), flowbiteReact()],
 });
+
